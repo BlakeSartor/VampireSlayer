9576e36b5a741905a43245993eecc6ee8cd81523
Blake Sartor <blakesartor@gmail.com>
2020-09-27 22:29:50 -0500

    adding bolt lobby

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Bolt.Matchmaking;
+using UdpKit;
+using UnityEngine;
+
+public class Menu : Bolt.GlobalEventListener
+{
+    public void startServer()
+    {
+        BoltLauncher.StartServer();
+    }
+
+    public void startClient()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            string matchName = Guid.NewGuid().ToString();
+
+            BoltMatchmaking.CreateSession(
+                sessionID: matchName,
+                sceneToLoad: "Main"
+            );
+        }
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
+
+        foreach (var session in sessionList)
+        {
+            UdpSession photonSession = session.Value as UdpSession;
+
+            if (photonSession.Source == UdpSessionSource.Photon)
+            {
+                BoltMatchmaking.JoinSession(photonSession);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        // randomize a position
+        var spawnPosition = new Vector3(Random.Range(-5, 5), 0, 0);
+
+        // instantiate cube
+        BoltNetwork.Instantiate(BoltPrefabs.Slayer, spawnPosition, Quaternion.identity);
+    }
+}
