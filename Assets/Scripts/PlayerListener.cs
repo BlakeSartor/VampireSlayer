6899a28f7cc248a18d75939f0180411b7c402ab7
Blake Sartor <blakesartor@gmail.com>
2020-10-03 18:52:02 -0500

    ADding hp bar and respawn mechanic

--- Assets/Scripts/PlayerListener.cs
+++ Assets/Scripts/PlayerListener.cs
@@ -1,21 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerListener : Bolt.GlobalEventListener
 {
 
-    public float health = 3f;
+    public HealthBar healthbar;
+
+    public float maxHealth = 10f;
+    float health;
     public override void OnEvent(TakeDamageEvent evnt)
     {
         BoltConsole.Write("EVENtttt HEARD");
         TakeDamage(evnt.Damage);
     }
 
+    public void Start()
+    {
+        health = maxHealth;
+        healthbar.SetMaxHealth(health);
+    }
+
     public void TakeDamage(float amount)
     {
         BoltConsole.Write("Healt: " + health);
         health -= amount;
+
+        healthbar.SetHealth(health);
 
         BoltConsole.Write("Healt after: " + health);
 
@@ -29,5 +42,12 @@ public class PlayerListener : Bolt.GlobalEventListener
     void Die()
     {
         BoltNetwork.Destroy(gameObject);
+        var spawnPosition = new Vector3(Random.Range(-5f, 5f), Random.Range(15f, 30f), 0f);
+
+        var player = BoltNetwork.Instantiate(BoltPrefabs.FirstPersonPlayer, spawnPosition, Quaternion.identity);
+
+        health = maxHealth;
+        healthbar.SetHealth(maxHealth);
+
     }
 }
