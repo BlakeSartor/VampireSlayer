88aa5ccc43fb97818a17ba2b787c32e1a26ba2fa
Blake Sartor <blakesartor@gmail.com>
2020-10-04 02:32:12 -0500

    adding pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject crosshair;
+    public GameObject pauseMenu;
+    public static bool gameIsPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        crosshair.SetActive(true);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        crosshair.SetActive(false);
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        BoltNetwork.Shutdown();
+        Application.Quit();
+    }
+}
